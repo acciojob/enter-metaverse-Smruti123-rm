cc16b557b51d6affa6ccf1eaa07ebbfc897ee360
Smruti Ranjan Mishra <140094639+Smruti123-rm@users.noreply.github.com>
2026-08-23 19:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const enterBtn = document.getElementById("enterBtn");
+    const status = document.getElementById("status");
+
+    enterBtn.addEventListener("click", function () {
+      status.outerHTML = "<h1 id=\"status\">Entered Metaverse</h1>";
+    });
